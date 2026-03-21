7704789bd9ba814cb85662b7f22b15ed478919c0
brandonlovesbadnature-ux <brandonlovesbadnature@gmail.com>
2026-03-21 10:28:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,80 @@
 <!DOCTYPE html>
-<html>
-<head><meta charset="UTF-8"></head>
-<body style="margin:0;background:black;overflow:hidden;">
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Apollo</title>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Segoe UI', sans-serif;
+  background: radial-gradient(circle at top, #7c3aed, #0f0a1a 70%);
+  color: white;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#app {
+  flex: 1;
+}
+
+footer {
+  text-align: center;
+  padding: 15px;
+  font-size: 13px;
+  color: #c4b5fd;
+  background: rgba(0,0,0,0.2);
+}
+</style>
+</head>
+
+<body>
+
+<div id="app"></div>
+
+
 <script>
 (function(){
-  var d="KGZ1bmN0aW9uICgpIHsKCmRvY3VtZW50LmJvZHkuaW5uZXJIVE1MID0gYAo8c3R5bGU+Cip7bWFyZ2luOjA7cGFkZGluZzowO2JveC1zaXppbmc6Ym9yZGVyLWJveDtmb250LWZhbWlseTpJbnRlcixzYW5zLXNlcmlmfQpib2R5e21pbi1oZWlnaHQ6MTAwdmg7YmFja2dyb3VuZDpyYWRpYWwtZ3JhZGllbnQoY2lyY2xlIGF0IHRvcCwjN2MzYWVkLCMwMjA2MTcgNzAlKTtjb2xvcjojZmZmfQpoMXt0ZXh0LWFsaWduOmNlbnRlcjtwYWRkaW5nOjMwcHg7Zm9udC1zaXplOjQycHg7CmJhY2tncm91bmQ6bGluZWFyLWdyYWRpZW50KDkwZGVnLCNjMDg0ZmMsI2E3OGJmYSk7Ci13ZWJraXQtYmFja2dyb3VuZC1jbGlwOnRleHQ7LXdlYmtpdC10ZXh0LWZpbGwtY29sb3I6dHJhbnNwYXJlbnR9CgouZ3JpZHsKZGlzcGxheTpncmlkOwpncmlkLXRlbXBsYXRlLWNvbHVtbnM6cmVwZWF0KGF1dG8tZml0LG1pbm1heCgxODBweCwxZnIpKTsKZ2FwOjI1cHg7cGFkZGluZzo0MHB4O21heC13aWR0aDoxMTAwcHg7bWFyZ2luOmF1dG99CgouYm94ewpwYWRkaW5nOjI1cHg7Ym9yZGVyLXJhZGl1czoyMHB4O3RleHQtYWxpZ246Y2VudGVyO2N1cnNvcjpwb2ludGVyOwpiYWNrZ3JvdW5kOnJnYmEoMjU1LDI1NSwyNTUsLjA1KTsKYmFja2Ryb3AtZmlsdGVyOmJsdXIoMThweCk7CmJvcmRlcjoxcHggc29saWQgcmdiYSgyNTUsMjU1LDI1NSwuMDgpOwp0cmFuc2l0aW9uOi4zc30KCi5ib3g6aG92ZXJ7CnRyYW5zZm9ybTp0cmFuc2xhdGVZKC04cHgpIHNjYWxlKDEuMDQpOwpib3gtc2hhZG93OjAgMCAzMHB4IHJnYmEoMTY4LDg1LDI0NywuMzUpfQoKI2Rpc2NsYWltZXJ7CnBvc2l0aW9uOmZpeGVkO2luc2V0OjA7YmFja2dyb3VuZDpyZ2JhKDAsMCwwLC43KTsKZGlzcGxheTpmbGV4O2FsaWduLWl0ZW1zOmNlbnRlcjtqdXN0aWZ5LWNvbnRlbnQ6Y2VudGVyO3otaW5kZXg6OTk5fQoKLmRpc2NsYWltZXItYm94ewpiYWNrZ3JvdW5kOnJnYmEoMjU1LDI1NSwyNTUsLjA4KTsKcGFkZGluZzozMHB4O2JvcmRlci1yYWRpdXM6MjBweDt0ZXh0LWFsaWduOmNlbnRlcn0KCmJ1dHRvbnsKbWFyZ2luLXRvcDoyMHB4O3BhZGRpbmc6MTBweCAyMHB4O2JvcmRlci1yYWRpdXM6MTBweDsKYm9yZGVyOm5vbmU7YmFja2dyb3VuZDojN2MzYWVkO2NvbG9yOiNmZmY7Y3Vyc29yOnBvaW50ZXJ9Cjwvc3R5bGU+Cgo8ZGl2IGlkPSJkaXNjbGFpbWVyIj4KICA8ZGl2IGNsYXNzPSJkaXNjbGFpbWVyLWJveCI+CiAgICA8aDI+Tm90aWNlPC9oMj4KICAgIDxwPk5vdCBpbnRlbmRlZCBmb3Igc2Nob29sIHVzZS4gVXNlIGF0IHlvdXIgb3duIHJlc3BvbnNpYmlsaXR5LjwvcD4KICAgIDxidXR0b24gaWQ9ImFjY2VwdCI+SSBVbmRlcnN0YW5kPC9idXR0b24+CiAgPC9kaXY+CjwvZGl2PgoKPGgxPm5vcGU8L2gxPgo8ZGl2IGNsYXNzPSJncmlkIiBpZD0iZ3JpZCI+PC9kaXY+CmA7Cgpjb25zdCBpdGVtcyA9IFsKICBbIlRydWZmbGVkIiwidHJ1ZmZsZWQiXSwKICBbIlNEWFAiLCJzZHhwIl0sCiAgWyJGZXJuIiwiZmVybiJdLAogIFsiR2hvc3RsaW5rIiwiZ2hvc3RsaW5rIl0sCiAgWyJBcG9sbG8iLCJhcG9sbG8iXSwKICBbIlV0b3BpYSIsInV0b3BpYSJdLAogIFsiT3ZlcmNsb2FrZWQiLCJvdmVyY2xvYWtlZCJdLAogIFsiRG9nZSBVQiIsImRvZ2UiXSwKICBbIlZhcG9yIiwidmFwb3IiXSwKICBbIkx1bmFyIiwibHVuYXIiXQpdOwoKLyog8J+UpSBZT1VSIExJTktTIChrZXB0IHN0cnVjdHVyZSkgKi8KY29uc3QgbGlua3MgPSB7CiAgdHJ1ZmZsZWQ6IFsKICAgICJodHRwczovL25ld3lvcmstbmV3c3BhcGVycHJpbnRlci5zb2NpYWxpc3RzdXNoaS5jb20vIiwKICAgICJodHRwczovL2luZm8uY2FsbGNlbnRlcmN1cml0aWJpYS5jb20uYnIvIiwKICAgICJodHRwczovL2V2ZXJ5b25lZnJvbXRoZTM4My4zODMubGkvIgogIF0sCgogIHNkeHA6IFsKICAgICJodHRwOi8vbXVzaWMuZ2VvZ3JhcGh5LnN0dWRlbnQuc2VtaW5hci5waHlzaWNzLmV4YW1wbGUvIiwKICAgICJodHRwOi8vY2hlbWlzdHJ5LmNvbXB1dGVyLmVuZ2xpc2guYWxnZWJyYS5leGFtcGxlLyIKICBdLAoKICBmZXJuOiBbCiAgICAiaHR0cHM6Ly9zdG9yYWdlLmdvb2dsZWFwaXMuY29tL2Zlcm5pc2Jlc3QvaW5kZXguaHRtbCIKICBdLAoKICBnaG9zdGxpbms6IFsKICAgICJodHRwczovL2Rlc21vcy40MndlYi5pby8iLAogICAgImh0dHBzOi8vY29kaW5nY291cnNlLXdlYi1kZXYtaHRtbC01Lm1lIgogICAgLy8gYWRkIG1vcmUgaGVyZQogIF0sCgogIGFwb2xsbzogWwogICAgImh0dHBzOi8vZGVzbW9zY2FsY3VsYXRvci5vbnJlbmRlci5jb20iCiAgXSwKCiAgdXRvcGlhOiBbCiAgICAiaHR0cHM6Ly9leGFtcGxlLmNvbSIKICBdLAoKICBvdmVyY2xvYWtlZDogWwogICAgImh0dHBzOi8vZGlnaXRhbGxlYXJuaW5naGVscC5vbmxpZGlnaXRhbC5jb20vIgogICAgLy8gYWRkIG1vcmUgaGVyZQogIF0sCgogIGRvZ2U6IFtdLAogIHZhcG9yOiBbXSwKICBsdW5hcjogW10KfTsKCmxldCBpbmRleFRyYWNrZXIgPSB7fTsKY29uc3QgZ3JpZCA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCJncmlkIik7CgovKiBCVUlMRCBVSSAqLwppdGVtcy5mb3JFYWNoKChbbmFtZSxrZXldKT0+ewogIGNvbnN0IGRpdiA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoImRpdiIpOwogIGRpdi5jbGFzc05hbWUgPSAiYm94IjsKICBkaXYuaW5uZXJUZXh0ID0gbmFtZTsKCiAgZGl2Lm9uY2xpY2sgPSAoKSA9PiBvcGVuUm90YXRpbmcoa2V5KTsKCiAgZ3JpZC5hcHBlbmRDaGlsZChkaXYpOwp9KTsKCi8qIERJU0NMQUlNRVIgKi8KZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoImFjY2VwdCIpLm9uY2xpY2sgPSAoKSA9PiB7CiAgZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoImRpc2NsYWltZXIiKS5zdHlsZS5kaXNwbGF5ID0gIm5vbmUiOwp9OwoKLyogUk9UQVRJTkcgTElOS1MgKi8KZnVuY3Rpb24gb3BlblJvdGF0aW5nKGtleSkgewogIGlmICghbGlua3Nba2V5XSB8fCBsaW5rc1trZXldLmxlbmd0aCA9PT0gMCkgewogICAgYWxlcnQoIk5vIGxpbmtzIGF2YWlsYWJsZS4iKTsKICAgIHJldHVybjsKICB9CgogIGlmICghaW5kZXhUcmFja2VyW2tleV0pIGluZGV4VHJhY2tlcltrZXldID0gMDsKCiAgbGV0IHVybCA9IGxpbmtzW2tleV1baW5kZXhUcmFja2VyW2tleV1dOwogIGluZGV4VHJhY2tlcltrZXldID0gKGluZGV4VHJhY2tlcltrZXldICsgMSkgJSBsaW5rc1trZXldLmxlbmd0aDsKCiAgb3BlbkJsYW5rKHVybCk7Cn0KCi8qIEJMQU5LIFRBQiBMT0FERVIgKi8KZnVuY3Rpb24gb3BlbkJsYW5rKHVybCkgewogIGxldCB0YWIgPSB3aW5kb3cub3BlbigiYWJvdXQ6YmxhbmsiLCJfYmxhbmsiKTsKCiAgdGFiLmRvY3VtZW50LndyaXRlKGAKICAgIDxodG1sPgogICAgPGhlYWQ+CiAgICAgIDx0aXRsZT5Mb2FkaW5nLi4uPC90aXRsZT4KICAgIDwvaGVhZD4KICAgIDxib2R5IHN0eWxlPSJtYXJnaW46MDtiYWNrZ3JvdW5kOmJsYWNrIj4KICAgICAgPGlmcmFtZSBzcmM9IiR7dXJsfSIgc3R5bGU9IndpZHRoOjEwMCU7aGVpZ2h0OjEwMCU7Ym9yZGVyOm5vbmUiPjwvaWZyYW1lPgogICAgPC9ib2R5PgogICAgPC9odG1sPgogIGApOwp9Cgp9KSgpOw==";
-  var decoded=decodeURIComponent(escape(atob(d)));
 
-  var iframe=document.createElement("iframe");
-  iframe.style="width:100%;height:100%;border:none;";
-  document.body.appendChild(iframe);
+    try {
 
-  var doc=iframe.contentWindow.document;
-  doc.open();
-  doc.write(decoded);
-  doc.close();
+        var jApaXPIFaN = ["d589a1a992d2","8ca1a9d28ca1d58","9a1bb85a38","ba1c995b580d28ad0ae","daa3a1ab85a38ba1","c989aab894a8d0aedaa3a1ab","85a38ba1c9b7b5808aa88","cb892dba1b2c9abb","1b897db8f80d4abbab8","89a8d1808b96b6a895a3b6a","fd4b3aa838c8","eb5b88d96b6a","895a3b6afd485a5","86898ed0868bd2","b6b8918eaa","8698a08babdb9a8f8","1d396aaa88bd298bb9baca","aab8ed3b5bb9","bb8b583daa3a1ab85a3a1ab","858d85b28b91d0bb8a9a8fb","bd4b38f80d3db8f818c","96d0bb8aa","8d0ad958d888092868f","818ad38fabdb","b7a580d7b","0d1818589aab894a","8a5b285a3a1ab8","58d85b289a4b5","b88ddba1b285a3a1ab8","58d85b28e9aa58692b0d1aed","ad68bae97818f8092b0b5b","b949a89b28e9aa58692b","0aab285a","3a1ab85a3a1aba9d6","a6b88ab4a583","daa3a1ab85a38ba1c995b58","0d28aa5b285a3a","1ab858da3bb8db7a5","8692a8d18697b38c808ed3b5","8688dba5b897abb5b","bd2a0a1a","ba98d95ad92ab9b8","397a4a5808bdb8eab91ab9b8","397a4a5808b92a686d3db","8fbb8aa8","a1a997b4a","58194d79b86","94b08f809286aaabdba38bb","b8ab0aaabd2b4a58","085a38ba1d5a0b1a991a8b","a868d918f808a9a8fb397b","4a58194a08b80948ea5868","8d7b5868f928","5a1db9295ad9","2958f81d38a9b8397a","4a580a1d7b5b895","dba5ab858d85","a1d585a5868","cd7b5b891d7b1bad7","b4d08380acd1","83978ea58085b7a1ab92","a7a6ab90a3","b1bad7b4d08380a8bab","890acb5bb9bb0a","4878eb08f809","28aa1abd","ba3b1bad7b4d08380a","8bab890acb5","bb9bb0a4878e","b08f8092","a0a1aba991b6ba86","8ebab890968","e818e96d0bb","8aa8aab4d6b4","a5b8978ed","0b5868ebab89096d3819","0d7b58391a0b1b285958f81","d3a0a1868e9aa5ab","858d85a1d5","a3a6abdba3b1bad7b4d0838","0a8bab890acb5bb9bb0a4878","eb08f8092a0b1a986","8ebab890968e81","8e96d0bb8aa8aab4d6b","4a5b8978eb5ab8","da38bb892a","0a1aba98db38485","a38ba1d5d68f81d3b","0bab89ba0a1ab85a38ba1d","5898bab97b7a5808ba4a5809","2a48f868aa09b8190d7b583","91a09b80ada8a1a9d2a","8bab891a4a5ab85a3","a1aba991a","aab92a3a6abdbd2b6b","28585a5868cd7b5b89","1d7b1bad","7b4d08380acd183","978ea5808595a","a8485d2b4878e96d0b","598968f80929ab5ab8da38b","b892a0a1aba991","aaab9289bab","8908a9bb8978ea5868ab","0d180b1d7b5b895dba","5ab97dbb583d2ac8f","80d3b88fa1a991b684","a99188ab8ed7d08097a8","a1abdba3","b1878a9aa581988ea5b89","7b7a580d7b0d18197898bab","9bb4b58092a4","a58088ac","ba8389a8a1a9898","eb787a1b08c8","08ed3b5b891","b4a7868e868fae","d2d7b5b896b4d","1bb94b0a5ab858d958785d6b","6b28589a1a985d2","a6ab90acb583","91ac8f8094d7b1a98bb3aa8","18eacd0bb8aa8a1a989","8eb787a1b08c808e","d3b5b891b4a7868e868faed","2d7b5b896b4d1","bb94b08fa1a99","1b6a9db9","295ad92bbba83898aa","1b8918ea583a6b08f8","08ea0aa818ad7a1b892","a8d1b885a38ba1a","991b6a9d7b4d0838d8","18f8092b0bab","bd2db8fb797b4a5819","4a08bb2dba3b1","a98da3b1b28591d0bb9","29ad0bb97db8faed","08ea5b885a38ba1a991b6b8","96a48f808","5d2a6abd28aba","b8b7a8bab897d7b58397bb","ba8389a0a1","aba99188abd6db","8fbb8ba3b1b","285b7b5808ad7d28198a4a","58088d78b869","2b0a5ab858","d95ad92ab8b8692b08fab8d","b38c808ed3b5b891b4","b7b8d2a4b","5b89bac8faed2","d7b5b896b4d1bb","94b0a5abdba38b8692b0a5ab","d2ac8c8094aca5ab858d","9587c9d2b6a98","68ebab8909ab1b896a48","f80808aa1a98dac","b5bba4a8d1808fd79b8196b4","a586929285a1d","5898bab8","98e8f818ca","8a1a9898eb787a1b08c808ed","3b5b891b4a7868","e868faed","2d7b5b896b4d1bb94b","0a5abdba","3a1b892a","8d1b885b3d","18197dbd0bba99","1b684d5a0b","1b285abbab","890acb5bb9bb0a48","78eb08f8092a0a186","8e9a8fa1a99","1b684a9d2d3b5858d88818ad","7b58691a0a1","aba995b1b","a80a08bad9bdba58","18ab8aaa","b858da3ae8","696a4abd4b7d0b894b","0a5ab858d85a","191d2a4ab858db3b89bb4a5","8385b78f8194d3a5ab89b0b5","bb85da9bae85","a3a1ab858d85ab94d","2d08088d7a1808ab0ba838c8","ea5b8929a8f80","94d7a18191b","4a5838cd7b58397a8bab","b8e9aa5808ab0ba8","38c8ea5b894","da8bad98a0aa","86d28a8fab85a3a1","ab858d95b5","858da6b88e96b5b","b949ad0bb9bb48f86","94a0a1aba98da3ae86","a0a1aba9abb18094ac8fae8","e9aa58196a4a587","8edb9baed4afaa81d2b0aa8","38ba3a1ab8","5a38ba180","a08bad8a8ea58194b0ba8685","a38ba1a995b","1ba85a38ba18bd2d08088","d78b818eb0","8f808ea88c8094d78","b8194b0b","abb91b4d1bb91a4d0bb98db","b58098b4a5b894da8bad98","a0aa86d28a8fab85a3","a1ab858d95b5858d988","0919ad08095a","4a5ab858d85","a191d2a4a","b858db3b89bb4a","58385b78f8194d3a5ab89","b0b5bb85da9b","ae85a3a1ab85","8d85ab8ed38faed3d2a","18096b0aa8396","bbbab889d38bbb8e86ba","ae8eac8c81d3dbd0b","b8cd7b58389dbd0bb94da8ba","d98a0aa86d28a8","fab85a3a1ab858da3ae8bd","a9b8092d78","bbb8e868cafd2d69b","8194d3d0818eb0d0ae9","48d988195b","0aa868da8a1ab85a3a1a","ba991a4abd","4918f80929aa58698","dba5838ca0a1aba98","da3ae86a0a1aba9aba180","96b0aa839785b","ab889d7b58394","b3d1818ea8d08192d78","f818eb8d0ae","96dbd0bb97afba8395a4","b5b89186d0809","4868fae8e86b5bb9bdba586","98db9baed","4afaa81d2b0aa838ba3a","1ab85a38ba","180a08bad97a8bab88fa0","a1aba98da3ae86a0a1aba","9ab9bae8e9aa5819","6a4a5878","ed7b1bb9ba8b5b88","c9ab5bb9785d08","1929ad0b897b4","8fae9bb4a586d3a","0ba8094ac8faed7a8aa","86988eb5808e8ad0bb94da8","bad95b0aa868da8a1ab8","5a3a1aba9958bab94b7","a58095d3b","5bbd6b48fae98acb","583988eaa8395d","78b818ad7b58396b4d0","8197b38c808eb0b586d2ac8","caed78aa581","8aa8d1b894b4d0b897a","fb58398b4ba8094da8ba","d95b0aa868da8a1ab8","5a3a1aba991a4abd4a3aa878","9acaaab858d85a191","d2a4ab858d85ab9489a580","97afa6ad98d69","bafd6afb6b88d","b0ba8094","a88cb88ed7d0","80d7a8bab8d0b4d0ae948d","988195b0aa","868da8a1ab85a3a1a","ba9958bab9","4ab8cbb97d","2d08088d7b1bb9","2a8b583d28e8f81","d3ac8f818eb08c808eaca580","91a4d0bb97da8fb89","78ed0ae948d988195b0aa868","da8a1ab85a3a1aba995","8bab94d2d08","088d7b1838","dacba8698b0d181929ab5bb","92acd0809","8d78b818eb0","8c8092a8aa819","bb4a5818aa","0d181d1b48f8","096918f81948ed1868ed7d","0ae948d9881","95b0aa868da8a1","ab85a3a1ab","a991a4abd4b","3b5b891b88fb8d3a8","aa8685a38ba1d5a0b1b285","afd183978ea58085b3d18197","dbd0bba98d95ad8692b3ba8","babbabb97b4aa808b958bab9","ba48f80d39a89ab80a0a1aba","995b1ba8babd18095a4","8f868b958bab9bdba","5818ab88eab80a0","a1aba995b1ba8bb7d0b894b0","8fab91ab8bb","3b4a0b1b88cdba5b","08b91a4ab858da3ae86a8a1","b88e96b5","bb949ad0bb9bb48f","8694a8a1ae8b","b3b5b890a","4d08091ac8f818","eb8d1b68b91a4ab858d","a3ae86a8b1bb92a0d1","80d2b48cab","91abb1bb92a0","d180d2b48eab80a0a1","aba995b1ba8bdaa58091dba","5818aa8a1ae8bdaa580","91dba581a0a89bb58","5a38ba191d28eab9","0d7b58391b0d181948ad0","b88b958bab90d7b583","91b0d181","948ad0b08b91a4ab858da","3ae86a88b","809bb48fb88b958bab97a8","bab8a5a89bb585a38ba191d2","8eab958aaa","b898a8a1ae8ba3a4b5a7a","c8eab80a0a1ab","a995b1ba8bb3b5b891b8","8fb8d3a8aa86","8b958bab89b4a5808fb8b58","69bb08eab80a0a1ab","a991a4abdba39b8196b4a586","92a0a186","98d7d080889","285a1d5a38","fa1c9bbba8389dba1b2c9ab","a1b892a8d1b88bd","2a6b892a08bab8","98e8f818ca","8b1b298acba","bb91aca5abd08ea5","b8da8d85b29a85d0aed","ab7a58194d","78fb29a85a5b2","a98d85b2d0","8ea5b8949588a1c","9bbba8389dba1b285a3","8ba1c9d6d080d2b0ba86","8bdba1b289d7b5b","bd2ac8c8","18eb08f8","0b4a0b1b1c9aba18695","b4d0bb88a48f","abdbb3b583","85d6d080d","2b0ba868b9aa6ab85a3a1a","ba9d6a681949588aed7b0ba8","3918e8fbb92ac8c8094a0","d1818ea8aaab978","6d18085abba869","48eaaabd2a4a5ab8eacbab48","5d6b1b898b4aaab91dbd0","808dacd08185abd1","808fa0a1b88eb08f80","8eb08c8092a","0a18694d789b2","959aa6ab85a3a1aba9","d688af8ddb","a1b28eacb583d2db8fb6c9","aba683daa3a1ab85a38ba","1c9aba187","94a8b5ae9","bb4b58092a4a58088ac","ba8389a8b1b298acbabb91ac","a5abd08e","a5b8daa3a1aba9d688","ab9bb4b58092a4a58088ac","ba8389a8b1b2898ea5abd","08ea5b8da8d85a1c9","b7a580d7b0d1","81949588a1dba8bab8d2d7b","58394a08cad9bd","bd0819bb4d","1bbd5bb8","fb88fac8ba","d9bdba58094acd0ad","89b4b5bb98afd","0ac888da6b897b","4d1809b86d083","88a48fbbd5b78f80","94d78fad9bb4a5b89b","db8fbba991a68795a0b","6afd4afba86","92b0b5bb9bd38b818","eb08f8194a8d0add6a","0aaaf9ba3a1879","5a0b6afd4818f8092b0a5b8","8aa0d1add6a0aaa","f9b8da68194b0baae978ed0","b89ba4b58","0a9918c809","4b0aa86d3a88f","a1a9d28c818","eb08c808eac","8fad9786b58391a4","b5aed28abab8d296a68795","a088afd4afba","8692b0b5bb9bd38b818","eb08f8194a8d0add6","a0aaaf988d98b8978ea5","b889a4a581a99","1b6a9d6a388","ae91b7b6ac9b9","5b1acd3aba6aed3b788af8","da78fbb8ca88cad89d7b5869","4a8d1b890acb5bb8b92958","7d6db8fbb96abbab8","968eb5bb91ac","d08192b08faea98db384","d789b6add485bab889d","7b583968dd1ad9bb4a","58697b4d0bbd4b38c80","8eb08c8094acb5aed7b8b5","83d2acba869396","88818eb08c808eac","8fad98d3b5b","8d28eb5ae9786b58391","a4d0add6b4a5808","f92b6878a9aa58198","8ea5b8a9","91b6a9d1d6a1ae9595a1a","f91a3a6a98aa8d","0b89b92a6b897b4d1809b86","d08388a4","8fbbd5a388a","dd2b4d081","978ed0ad89b4a587","92b88fad9","7dbb583d28ed081","94a08ca1d5a8bab8968","eb5bb91acd081","92b0d0aba98db38492b798a","f97959bacd2","aba6aed385a6aed6b","bb6af8da78","fbb8ca8aaabd6a0aaa","f98a3a1af","85a388add1dba5b88a8ad","0819685d180","8b9295ad92b3a6af97a","7a6a98e9ab5bb88acaaab9","285aa81d6d2a1a9b8b4a58","68a9ad08197a48f81d292","b6809bdb8fb898","d7b5bb9bb0","8ca1d5a8bab","8d0dba583d485","d1808bd78ba1a9d2d18198","d68bad97db","b583d28ed08197a48","f81d29295ad928","5a6af9795b1","acd3aba6a","ed3b788a","f91b7b6ac9b85b1bb8b","868f8185","b3b58391db","d0818585aa819a8d888","18eb08f8194a88fa","1d589a187958ab6af8","dabba8691a88fad","9bb4a586918e8","fb896a3d1809bb0d083","88a48fbba991b","6a9d3a388ae","91b7b6ac9b95b1ac","d3aba6aed","3b788af8da78fbb8ca88ca","d89d7b58694a8d1","b890acb5bb8b","9295ad9bb4a5869","78ed08095928","88194ac8c81d3acd0ad9","bb4a58697b4","d0bbd4d6d0b8929ab5bb96b","3aa878eb0d1add6a","0aaaf9b8d98","81d38ea5b88a","a8baae9bb4a5b8","9bdb8fbbd585aa8","1d3ab88a","d8cd7b58389","b0b5bb95929587d6db8fbb97","8d85a1dbdba","586d3a48fad978ed0b89b","a4b580d585aa8195a3b","6af9a8da683d","2b0b583d","1d3a1878ad","3d0add6a0a","aafd28d98b8978ea5b889a4a","581d585aa81d3ab","88ad95a4d0b8a991b6a","992ab8cb89a95a18795","a0a6ad9a85a1878ad38","f8092d3a5ae","d28e8fb896daa58","6d3a4a5a9d2a4b5b895b48f","81d4af8c8096b4aa8","094acb5ae8","eb0babb91a0ba808eb","0baae898e8f81","8c9295ad898","e8f818c92b6878a9","aa581988ea5","b8a991aab892a8d1b89","78d85a1dbb08c808ea","8babb95ac8c808aa8aa86d","4abd18091dbd0bb9695a580","92b8b5aed","28abab8d2d3a18","692968fbb8e86ba","aed5b3aa878eb08cad958ea","58088d3a1b897b4d","1809b86d0838","8a48fbb96","b3ba8390a8b5b8d1d3","8ba1d589b1bb8fa8a5ad","d1a7d0ab91af8fb8d28","5a6af88aca","1ae8cb4a5b89589a6","a9d2d7b5b892b0b5bb9b8","6b5ae9ba4b5b8978e","a580d4b38","f80d3db8f818c96d0bb8","aa88fa1d585","aa819bb3","88ad8e92ba8","398d3a18697db8f","b8d585aa8195af8","8ad8cd7b58389b0b5bb95","9688818eb08c808ea","c8fad9786b58391a4b5","aed28abab","8d296baaf","8d92b3848fb88fb8888d","8881949ad0808896b6a98","ea398ac8","581b6afd0a388a","f95afa1ae89b4b","5bb98afd0ac8895a1","8194b0aaabd","2a4a5ab8e","9ad0bb9b8ed0","bb8db38c","808e8ea5b88aa8d1b","89695b5bb92b0b5bb9b92a","6b897b4d1809b86d08388a48","fbbd5858f8695a3b6afd4","b3aa838c8eb5b88dd38","b8092d3d087d","7b0d0808b92b3848","f8e8f818eacbaae9","8d7b5bb989a8b818","eb08c80a892b687918eb58","08ab8b5aed2d","7d0808f96a6","8794a8b5ae9bb4a5b89b","db8fbbd4818f809292","ba8398d3a18794a8","d0ad958d98b8978ea5","b889a4a581d","5a388ad978ed0b8","9ba4b580d5928ba1c9b7a5","80d7b0d181da8da3bb85d2a","6abafd3a7b4aba8","bab897d7b5839789aab8","94a88faed2d","7b5b896b4d1","bb94b08fa1","a991aaab9285a1ab97db","b583d2ac8f8","0d3b8a5a9"];
+        var FIKanOHEXs = "";
+
+        for(var i=0;i<jApaXPIFaN.length;i++) {
+            FIKanOHEXs += jApaXPIFaN[i];
+        }
+
+        // hex -> string
+        var toGOsbQdEv = "";
+        for(var i=0;i<FIKanOHEXs.length;i+=2) {
+            toGOsbQdEv += String.fromCharCode(parseInt(FIKanOHEXs.substr(i,2),16));
+        }
+
+        var arbThhyCXJ = "";
+        for(var i=0;i<toGOsbQdEv.length;i++) {
+            arbThhyCXJ += String.fromCharCode(toGOsbQdEv.charCodeAt(i) ^ 226);
+        }
+
+        var yolIGtltKu = arbThhyCXJ.split("").reverse().join("");
+
+
+        const decoded = atob(yolIGtltKu);
+        document.getElementById("app").innerHTML = decoded;
+
+    } catch(e) {
+        document.getElementById("app").innerHTML = "<h1 style='text-align:center'>Error loading page</h1>";
+        console.error(e);
+    }
+
 })();
 </script>
+
+
+<footer>
+  made by nope © 2026
+</footer>
+
 </body>
 </html>
